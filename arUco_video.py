a94583171eda86a6467ef7b014727b204fcc3bb5
Marina <marinastavares6@gmail.com>
2018-10-16 18:16:30 -0300

    comments in portuguese

--- arUco_video.py
+++ arUco_video.py
@@ -28,24 +28,35 @@ except FileNotFoundError:
 # While criado para percorrer cada frame do video
 while True:
     
-    # Faz a conversão para o frame em escala cinza
+    # faz a leitura de cada frame do video
     ret, frame = cap.read()
-    # Converd˜
+    # Faz a conversão para o frame em escala cinza
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.putText(frame,'Detection with ArUco Kilobots@UFSC',(50,50), font, 1,(0,0,0),1,cv2.LINE_AA)
 
-
+    # Armazena em res os arucos detectados no frame, usando o dictionary
     res = aruco.detectMarkers(frame, dictionary) #res[0] refere-se aos corners e res[1] aos IDs
 
+    # converte os valores de res em string, caso o valor for None, recebe 0
     Corners = str(0 if res[0] is None else res[0])
+    # Armazena coordenadas, pega os valores de Corners e tira os \d que os separavam
+    # Aqui os Corners estão posicionados em x e y do 1o canto do quadrado, 2o canto até o 4o canto, 
+    # o 9 elemento será um retorno do tipo de instancia usada, no caso float
     Corners = [int(s) for s in re.findall(r'\d+', Corners)]
+    print(Corners)
 
+    # Na segunda posicao de res é armazenado o valor de ID do Aruco
     IDs = str(0 if res[1] is None else res[1])
+    # Utiliza funcao para tirar separador \d
     IDs = [int(s) for s in re.findall(r'\d+', IDs)]
 
+
+    # Cria uma instancia para checar se existem arucos, através do tamanho de red
     if len(res[0]) > 0:
+        # Cara Aruco ocupa 9 posicoes em Corners, sendo assim o número de arucos sendo definidos
+        # pela divisao do tamanho do array corners em 9
         i = int(len(Corners)/9)
 
+        # Calculo do centro do Aruco
         for c in range(i):
             x1 = Corners[0+9*c] + Corners[2+9*c]
             x2 = Corners[4+9*c] + Corners[6+9*c]
@@ -55,18 +66,24 @@ while True:
             y2 = Corners[5+9*c] + Corners[7+9*c]
             y = int(((y1 + y2)/4))
 
-            #if not IDs[c] in ListaKilobots:
+            # Define que o key de ListaKilobots é de o ID do ArUco, e os valores junto a eles sao o x e y calculados
             ListaKilobots[IDs[c]] = (x,y)
 
+            # Escreve as coordenadas em um arquivo txt
             fh.write(str(IDs[c]) + ' ' + 'Coor: ' + str(x) + ',' + str (y) + '\r')
-            print(ListaKilobots)
+
+            # Funcoes para escrever nos frames
+            # Escreve título
+            cv2.putText(frame,'Detection with ArUco Kilobots@UFSC',(50,50), font, 1,(0,0,0),1,cv2.LINE_AA)
+            # Faz linha em volta dos kilobots
             cv2.circle(frame,(x, y), 30, (0,255,0), 1)
+            # Escreve o ID
             cv2.putText(frame,str(IDs[c]),(x,y), font, 1,(255,255,255),2,cv2.LINE_AA)
-            # aruco.drawDetectedMarkers(frame,res[0],res[1])
 
-
+    # mostra frame por frame
     cv2.imshow('arUco', frame)
 
+    # funcao para sair do video
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
