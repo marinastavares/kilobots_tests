b5d7eb86d84a9e297c6ccaf43c5355fcfb9ad3b9
Marina <marinastavares6@gmail.com>
2018-10-19 09:52:35 -0300

    Add some phrases and button

--- testing_interface.py
+++ testing_interface.py
@@ -7,36 +7,36 @@ def code2():
 def sel1():
     global x
     x= var.get()
-    selection = "You selected the option " + str(x)
+    selection = "Voce selecionou a entrada " + str(x)
     x=int(x)
     label.config(text = selection)
     code2()
 
-def sel2():
-    global x
-    x= var.get()
-    selection1 = "You selected the option " + str(x)
-    label.config(text = selection1)
-    print(selection1)
-
-
 
 root = Tk()
+
+
+whatever_you_do = "Kilobots@UFSC"
+msg = Message(root, text = whatever_you_do)
+msg.config( font=('arial', 24, 'italic'))
+msg.pack()
+
 var = IntVar()
 R1 = Radiobutton(root, text="Webcam 1", variable=var, value=0,
                   command=sel1)
 R1.pack( anchor = W )
 
 R2 = Radiobutton(root, text="Webcam 2", variable=var, value=1,
-                  command=sel2)
+                  command=sel1)
 R2.pack( anchor = W )
 
 def close_window(): 
     root.destroy()
 
-button = Button(root, text="QUIT", fg="red",command=close_window)
+button = Button(root, text="Selecionar Entrada", command=close_window)
 button.pack()
 
 label = Label(root)
+label.config(text = 'Nenhuma entrada selecionada')
 label.pack()
 root.mainloop()
